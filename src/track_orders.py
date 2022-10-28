112916c3e767b2821a534abf258579f13959d100
kelsonbatista <kelsonbatista@gmail.com>
2022-10-28 11:10:37 -0300

    feat: requisito2 - track orders class

--- src/track_orders.py
+++ src/track_orders.py
@@ -1,22 +1,55 @@
+from collections import Counter
+
+
 class TrackOrders:
-    # aqui deve expor a quantidade de estoque
+    def __init__(self):
+        self._data = list()
+
     def __len__(self):
-        pass
+        return len(self._data)
 
     def add_new_order(self, customer, order, day):
-        pass
+        self._data.append({"customer": customer, "order": order, "day": day})
 
     def get_most_ordered_dish_per_customer(self, customer):
-        pass
+        items = []
+        for order in self._data:
+            if order["customer"] == customer.lower():
+                items.append(order["order"])
+        result = Counter(items).most_common()
+        return result[0][0]
 
     def get_never_ordered_per_customer(self, customer):
-        pass
+        items = []
+        for order in self._data:
+            if order["customer"] == customer.lower():
+                items.append(order["order"])
+        personal = Counter(items).most_common()
+        personal_set = [item[0] for item in personal]
+        orders_order = []
+        for order in self._data:
+            orders_order.append(order["order"])
+        total = Counter(orders_order).most_common()
+        total_set = [item[0] for item in total]
+        return set(total_set) - set(personal_set)
 
     def get_days_never_visited_per_customer(self, customer):
-        pass
+        personal_weekday = [order["day"]
+                            for order in self._data
+                            if order["customer"] == customer.lower()]
+        personal = Counter(personal_weekday).most_common()
+        personal_set = [item[0] for item in personal]
+        orders_order = [order["day"] for order in self._data]
+        total = Counter(orders_order).most_common()
+        total_set = [item[0] for item in total]
+        return set(total_set) - set(personal_set)
 
     def get_busiest_day(self):
-        pass
+        orders_order = [order["day"] for order in self._data]
+        total = Counter(orders_order).most_common()
+        return total[0][0]
 
     def get_least_busy_day(self):
-        pass
+        orders_order = [order["day"] for order in self._data]
+        total = Counter(orders_order).most_common()
+        return total[-1][0]
